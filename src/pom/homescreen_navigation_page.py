63be5a29745be41b590fef8773b5f195c8a26b8f
saikiran <saikiran@example.com>
2025-02-08 19:12:54 +0530

    Simple Change

--- src/pom/homescreen_navigation_page.py
+++ src/pom/homescreen_navigation_page.py
@@ -8,14 +8,14 @@ class HomePage:
         self.page = page
         
     def verify_homescreen_menu(self):
-        self.page.locator('//*[@id="headerPanel"]/ul[2]/li[1]/a').click()  # Example selector for the navigation menu
+        self.page.locator('//*[@id="headerPanel"]/ul[2]/li[1]/a').click()  
         assert self.page.title(),'| Welcome | Online Banking'
 
     def verify_emp_screen(self):
-        self.page.locator('//*[@id="headerPanel"]/ul[2]/li[2]/a').click()  # Example selector for the navigation menu
+        self.page.locator('//*[@id="headerPanel"]/ul[2]/li[2]/a').click()  
         assert self.page.title(), 'ParaBank | About Us'
 
     def verify_customer_screen(self):
-        self.page.locator('//*[@id="headerPanel"]/ul[2]/li[3]/a').click()  # Example selector for the navigation menu
+        self.page.locator('//*[@id="headerPanel"]/ul[2]/li[3]/a').click() 
         assert self.page.title(),'ParaBank | Customer Care'
     
